13af3523ec33584f206a9875884b9efb5854e3e2
Abhishek Kumar <67044381+abhi751@users.noreply.github.com>
2023-01-07 18:07:49 +0530

    updated login

--- src/Components/Login/LoginApp.js
+++ src/Components/Login/LoginApp.js
@@ -3,6 +3,7 @@ import "./LoginApp.css"
 import {useNvigate} from "react-router-dom"
 import {setToken,getToken} from "../../setLocalstorage"
 
+
 const URL=process.env.API_URL || "http://localhost:3000";
 
 const login=()=>{
@@ -10,6 +11,20 @@ const login=()=>{
     const[data,setData]=useState({
         email:"",
         password:"",
-    })
+    });
+    const[errormsg,setErrormsg]=useState("");
+    const[show,setShow]=useState("")
+    const[message,setMessage]=useState("")
+    const[passwordErrorMessage,setPasswordErrorMessage]=useState("")
+
+    const emailChangeHandler=(e)=>{
+        setData({...data,email:e.target.value})
+        if(!e.target.value.match(/[^a-zA-Z0-9]/)|| !e.target.value.includes("@")|| !e.target.value.endWith(".com")){
+          setErrormsg("Please enter a valid email id")
+      } else{
+        setErrormsg("")
+      }
+    }  
+
     
 }
